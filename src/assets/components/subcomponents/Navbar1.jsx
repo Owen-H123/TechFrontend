09a589ced11f5902383356d4eedfcd8b8f2bb951
Owen-H123 <hugokuro0@gmail.com>
2025-11-22 14:01:22 -0500

    cambio nav

--- src/assets/components/subcomponents/Navbar1.jsx
+++ src/assets/components/subcomponents/Navbar1.jsx
@@ -1,8 +1,13 @@
+import { FaUser } from "react-icons/fa";
+
+
 export default function Navbar1(){
     return(
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+
+    <div className="container">
+         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar w/ text</a>
+                <a className="navbar-brand" href="#">logo</a>
 
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -11,21 +16,29 @@ export default function Navbar1(){
                 <div className="collapse navbar-collapse" id="navbarText">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link active" aria-current="page" href="#">inicio</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Features</a>
+                            <a className="nav-link" href="#">funcionalidades</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Pricing</a>
+                            <a className="nav-link" href="#">beneficios</a>
+                        </li>
+                         <li className="nav-item">
+                            <a className="nav-link" href="#">contacto</a>
                         </li>
                     </ul>
 
-                    <span className="navbar-text">
-                        Navbar text with an inline element
-                    </span>
+                    
+                       <button className="btn btn-primary" type="button" style={{backgroundColor:"#319EA4",margin:"20px",paddingRight:"40px",paddingLeft:"40px"}}>
+                         <FaUser  style={{marginRight:"10px", marginLeft:"2px"}}/>
+                        Ingresar
+                        </button>
+                      
+                    
                 </div>
             </div>
         </nav>
+    </div> 
     );
 }
