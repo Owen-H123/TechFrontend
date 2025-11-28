8f250f665ed7b8274067b06c498088fd3011c7b0
Owen-H123 <hugokuro0@gmail.com>
2025-11-27 20:24:33 -0500

    Creando pagina de pedidos

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import Login from "./assets/pages/Login.jsx";
 import Registro from "./assets/pages/Registro.jsx";
 import Reportes from "./assets/pages/Reportes.jsx";
 import Catalogo from "./assets/pages/catalago.jsx";
+import Pedidos from "./assets/pages/pedidos.jsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/login", element: <Login /> },
   { path: "/registro", element: <Registro /> },
   { path: "/reportes", element: <Reportes /> },
-  { path: "/catalogo", element: <Catalogo /> }
+  { path: "/catalogo", element: <Catalogo /> },
+  { path: "/Pedidos", element:<Pedidos/>}
 ]);
 
 function App() {
